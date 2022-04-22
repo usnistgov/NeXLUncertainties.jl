3b840ee9a6e4c6122b03af86a05c0d4c404e9a73
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2022-04-22 08:16:43 -0400

    Rename flatten, unflatten => toarray, fromarray

--- src/labeledvalues.jl
+++ src/labeledvalues.jl
@@ -106,28 +106,28 @@ asa(::Type{DataFrame}, lv::LabeledValues) = DataFrame( Label = labels(lv), Value
 
 
 """
-    flatten(lv::LabeledValues)
+    toarray(lv::LabeledValues)
 
-`flatten(...)` and `unflatten(...)` are a pair of functions for making `LabeledValues`
+`toarray(...)` and `fromarray(...)` are a pair of functions for making `LabeledValues`
 compatible with functions that take as arguments a vector of Float64.  This is common
-for non-linear optimization functions.  `flatten(lv)` takes a `LabeledValues` and returns
-an ordered vector of Float64.  `unflatten(f,lv)` is a little more subtle as it returns
-a function `g` such that `g(flatten(lv)) = f(lv)`.  That is, `unflatten(...)` allows you
+for non-linear optimization functions.  `toarray(lv)` takes a `LabeledValues` and returns
+an ordered vector of Float64.  `fromarray(f,lv)` is a little more subtle as it returns
+a function `g` such that `g(toarray(lv)) = f(lv)`.  That is, `fromarray(...)` allows you
 to use `f(lv)` as an argument to functions that expect a vector argument.
 """
-flatten(lv::LabeledValues) = values(lv)
+toarray(lv::LabeledValues) = values(lv)
 
 """
-    unflatten(f::Function, lv::LabeledValues)
+    fromarray(f::Function, lv::LabeledValues)
 
-`flatten(...)` and `unflatten(...)` are a pair of functions for making `LabeledValues`
+`toarray(...)` and `fromarray(...)` are a pair of functions for making `LabeledValues`
 compatible with functions that take as arguments a vector of Float64.  This is common
-for non-linear optimization functions.  `flatten(lv)` takes a `LabeledValues` and returns
-an ordered vector of Float64.  `unflatten(f,lv)` is a little more subtle as it returns
-a function `g` such that `g(flatten(lv)) = f(lv)`.  That is, `unflatten(...)` allows you
+for non-linear optimization functions.  `toarray(lv)` takes a `LabeledValues` and returns
+an ordered vector of Float64.  `fromarray(f,lv)` is a little more subtle as it returns
+a function `g` such that `g(toarray(lv)) = f(lv)`.  That is, `fromarray(...)` allows you
 to use `f(lv)` as an argument to functions that expect a vector argument.
 """
-function unflatten(f::Function, lv::LabeledValues, constants::LabeledValues=nothing)
+function fromarray(f::Function, lv::LabeledValues, constants::LabeledValues=nothing)
     return if isnothing(constants)
         v->f(LabeledValues(labels(lv), v))
     else
